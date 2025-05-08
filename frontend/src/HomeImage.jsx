b18c60775adb9e76e950ba6f5f2eb508153690f5
Christine Doan <angelstudyblr@gmail.com>
2025-05-07 23:04:35 -0700

    adding a tiny bit of basic css

--- frontend/src/HomeImage.jsx
+++ frontend/src/HomeImage.jsx
@@ -4,7 +4,8 @@ import "./HomeImage.css";
 const HomeImage = () => {
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1>Dabloons</h1>
+            <h1>Greetings, Traveler.</h1>
+            <h2>Peruse as you wish.</h2>
             <img
                 src="https://preview.redd.it/dabloons-cat-v0-1nujqh9wdlwc1.jpeg?auto=webp&s=7d0ebd2a743325280abc8a5f1c727cc7283ac945"
                 alt="Dabloons Cat"
