4a8799eb300b4d3a156d9be7c4cad598fb1632a3
Christine Doan <angelstudyblr@gmail.com>
2025-05-09 12:53:46 -0700

    worked a bit on the navigation bar

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -5,7 +5,7 @@ import "./NavBar.css";
 const NavBar = () => {
     return (
         <nav className="navBar">
-            <div className="logo">Dabloons</div>
+            <img src="https://media.forgecdn.net/avatars/thumbnails/265/180/256/256/637228616324135331.png" className="logoImg"></img><div className="logo">Dabloons</div>
             <ul className="navLinks">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/products">Products</Link></li>
