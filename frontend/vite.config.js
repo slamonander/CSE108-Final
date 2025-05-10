028433d46062f210cf6ecd03ad01a26f4d65375a
Christine Doan <angelstudyblr@gmail.com>
2025-05-09 23:34:57 -0700

    fixing config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'  // this is what Vercel looks for
+  server: {
+    historyApiFallback: true
   }
 });
