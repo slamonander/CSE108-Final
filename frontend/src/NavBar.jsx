f4ddead76da3f956cf95b2149d56dbaf92e4ece1
Jack <jackpost100@gmail.com>
2025-05-11 20:47:34 -0700

    Revert "Purchase"
    This reverts commit ae59ef612300dc3994d1c97ff353f6ff02aaa57b, reversing
    changes made to 265cf596bf15fab57c9f22d212d160f29befc210.

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -14,8 +14,8 @@ const NavBar = ({ isAuthenticated, setIsAuthenticated }) => {
             <img src="https://media.forgecdn.net/avatars/thumbnails/265/180/256/256/637228616324135331.png" className="logoImg"></img><div className="logo">Dabloons</div>
             <ul className="navLinks">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/products">Items</Link></li>
-                <li><Link to="/cart">Basket</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
                 {isAuthenticated ? <li><button className="logoutBtn" onClick={handleLogout}>Log out</button></li> : <li><Link to="/login">Login</Link></li> }
             </ul>
         </nav>
