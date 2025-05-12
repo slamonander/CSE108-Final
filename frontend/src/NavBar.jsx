af295c925d29a105006279b4e39236d7fcce133c
Christine Doan <angelstudyblr@gmail.com>
2025-05-11 19:24:21 -0700

    try adding a little avatar to signify user's logged in

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = ({ isAuthenticated, setIsAuthenticated }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+            setUser(storedUser);
+        }
+    }, []);
+    
     const handleLogout = () =>{
         localStorage.removeItem('token');
         localStorage.removeItem('user');
@@ -16,7 +25,18 @@ const NavBar = ({ isAuthenticated, setIsAuthenticated }) => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/cart">Cart</Link></li>
-                {isAuthenticated ? <li><button className="logoutBtn" onClick={handleLogout}>Log out</button></li> : <li><Link to="/login">Login</Link></li> }
+                {isAuthenticated ? (
+                    <>
+                        <li className="avatar">
+                            <img src={user?.image || "https://64.media.tumblr.com/99e1a6e7924dd08f53a863840c6db23a/ddc47f5445fb968a-66/s500x750/8880abcb41ecb93cd8c9ddd109cb3ea6afe30783.gifv"}
+                            alt="User"
+                            className="avatarImg"/>
+                        </li>
+                        <li><button className="logoutBtn" onClick={handleLogout}>Log out</button></li>
+                    </>
+                ) : (
+                    <li><Link to="/login">Login</Link></li>
+                )}
             </ul>
         </nav>
     );
